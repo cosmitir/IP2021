8fa3f70a4f23ce718707df0009254a4ebc9232a1
cosmitir <66184056+cosmitir@users.noreply.github.com>
2020-10-15 18:59:54 +0100

    modified:   s050910/package.bluej 	modified:   s121610/a1310.java

--- s121610/a1310.java
+++ s121610/a1310.java
@@ -11,13 +11,14 @@ public class a1310
 
         public static void main(String[] args)
             {
-        System.out.println("IPB-ESTIG");
-        System.out.println();
-        System.out.print("Indicate a number: ");
-        int i = scanner.nextInt();
-        System.out.print("Indicate another number: ");
-        int l = scanner.nextInt();
-        int j = i+l;
-        System.out.println("The sum of the numbers = " + j);
+                System.out.println();
+                System.out.print("Indicate a number: ");
+                int i = scanner.nextInt();
+                System.out.print("Indicate another number: ");
+                int l = scanner.nextInt();
+                
+                int j = i+l;
+                
+                System.out.println("The sum of the numbers = " + j);
+            }
     }
-}
