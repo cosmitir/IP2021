d053a293f02381aa815d98525c8bf3b4afc1eeaa
cosmitir <66184056+cosmitir@users.noreply.github.com>
2020-10-22 14:59:24 +0100

    update

--- s192310/a2010b.java
+++ s192310/a2010b.java
@@ -18,18 +18,19 @@ public class a2010b
 
                 System.out.println("\nCálculo do consumo de gasolina");
                 System.out.println();
-
-                System.out.print("Indique a distância percorrida (em km): ");
-                distanciap = scanner.nextDouble();
-                System.out.print("Indique o consumo médio do carro (l/100km): ");
-                consumom = scanner.nextDouble();
-                System.out.print("Preço do combustível ($/litro): ");
-                precoc = scanner.nextDouble();
+                
+                distanciap = readDouble("Indique a distância percorrida (km): ");
+                consumom = readDouble("Indique o consumo médio do carro (l/100km): ");
+                precoc = readDouble("Indique o preço do combustível (euros): ");
 
                 consumo = (consumom * distanciap) / 100;
                 preco = consumo * precoc;
 
                 System.out.print("O consumo do carro em " + distanciap + "km foi de " + consumo +"L.\nO preço do combustível gasto é de " + preco + " euros.");
-
+            }
+        static double readDouble(String message)
+            {
+                System.out.print(message);
+                return scanner.nextDouble();
             }
     }
