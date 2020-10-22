346414ce2319ecffe21c9c735fb3ff4ac42f39ab
cosmitir <66184056+cosmitir@users.noreply.github.com>
2020-10-22 15:02:46 +0100

    update

--- s192310/a2010b.java
+++ s192310/a2010b.java
@@ -23,8 +23,8 @@ public class a2010b
                 consumom = readDouble("Indique o consumo médio do carro (l/100km): ");
                 precoc = readDouble("Indique o preço do combustível (euros): ");
 
-                consumo = (consumom * distanciap) / 100;
-                preco = consumo * precoc;
+                consumo = consumoF(consumom, distanciap);
+                preco = precoF(consumo, precoc);
 
                 System.out.print("O consumo do carro em " + distanciap + "km foi de " + consumo +"L.\nO preço do combustível gasto é de " + preco + " euros.");
             }
@@ -32,5 +32,13 @@ public class a2010b
             {
                 System.out.print(message);
                 return scanner.nextDouble();
+            }
+        static double consumoF(double consumom, double distanciap)
+            {
+                return (consumom * distanciap) / 100;
+            }
+        static double precoF(double consumo, double precoc)
+            {
+                return consumo * precoc;
             }
     }
